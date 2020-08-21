1c5d18e921b41ebcf99790b254baccedc5abb6aa
exp1hze <384877515@qq.com>
2020-08-21 15:05:33 -0500

    test

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        print("Hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
